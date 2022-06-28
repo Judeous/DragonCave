ef77c40b81b36028724247f616fba332ac9a1662
Judeous <judeous@tutanota.com>
2022-06-28 10:14:45 -0500

    Player can restart, displays score

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,6 @@
 package main;
 
+import java.util.Objects;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -11,7 +12,6 @@ public class Main {
         System.out.println("is greedy and hungry and will eat you on sight.");
         System.out.println("Which cave will you go into? (Enter '1' or '2')");
 
-        String scannerData = new String();
         Scanner scanner = new Scanner(System.in);
 
         int input;
@@ -19,21 +19,35 @@ public class Main {
             input = scanner.nextInt();
         } while (input != 1 && input != 2);
 
-        float chance = new Random().nextFloat();
+        boolean quit = false;
         do {
-            System.out.println("You've approached another fork. In front of you,");
-            System.out.println("you see two caves. In one cave the dragon is friendly");
-            System.out.println("and will share his treasure with you. The other dragon");
-            System.out.println("is greedy and hungry and will eat you on sight.");
-            System.out.println("Which cave will you go into? (Enter '1' or '2')");
-            System.out.print("> ");
-
+            int score = 0;
+            float chance;
             do {
-                input = scanner.nextInt();
-            } while (input != 1 && input != 2);
+                System.out.println("You've approached another fork. In front of you,");
+                System.out.println("you see two caves. In one cave the dragon is friendly");
+                System.out.println("and will share his treasure with you. The other dragon");
+                System.out.println("is greedy and hungry and will eat you on sight.");
+                System.out.println("Which cave will you go into? (Enter '1' or '2')");
+                System.out.print("> ");
 
-            chance = new Random().nextFloat();
-        } while (chance < .75f);
-        System.out.print("You got dead.");
+                do {
+                    input = scanner.nextInt();
+                } while (input != 1 && input != 2);
+
+                chance = new Random().nextFloat();
+                score++;
+            } while (chance < .75f);
+            System.out.println("You got dead.");
+            System.out.println("You entered " + score + " forks in the cave.");
+
+            System.out.println("Would you like to try again? ('y' or 'n')");
+            String data;
+            do {
+                data = scanner.nextLine();
+            } while (!Objects.equals(data, "y") && !Objects.equals(data, "n"));
+            quit = data.equals("n");
+            
+        } while (!quit);
     }
 }
